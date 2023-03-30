99e24ea98940cd80536d7e67a471cbe56ea84003
wss-sosen <127080498+wss-sosen@users.noreply.github.com>
2023-03-31 07:29:40 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -128,7 +128,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "好高兴与你相遇！我是ChatGPT,一个由infoepoch团队部署在网站的AI智能助手，我的中文名字叫做元元，与我对话需要点击左下角设置页面自行设置key，没有key请到首页获取，期待与您聊天哟！ https://www.infoepoch.cn ",
+    BotHello: "好高兴与你相遇！我是ChatGPT,一个由openAI开发并由infoepoch团队部署在网站的AI智能助手，我的中文名字叫做元元，与我对话需要点击左下角设置页面自行设置key，没有key请到首页获取，期待与您聊天哟！ https://www.infoepoch.cn ",
     Error: "出错了，稍后重试吧，返回首页：https://www.infoepoch.cn",
     Prompt: {
       History: (content: string) =>
