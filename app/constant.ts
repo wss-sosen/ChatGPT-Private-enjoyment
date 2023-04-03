c2613d37981b2b6822a46966881254d56760942f
wss-sosen <127080498+wss-sosen@users.noreply.github.com>
2023-04-03 20:36:15 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
 export const OWNER = "Yidadaa";
 export const REPO = "ChatGPT-Next-Web";
 export const REPO_URL = `https://www.infoepoch.cn`;
-export const UPDATE_URL = `https://github.com/wss-sosen/chatgpt-webs#%E4%BF%9D%E6%8C%81%E6%9B%B4%E6%96%B0-keep-updated`;
+export const UPDATE_URL = `https://www.infoepoch.cn`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/Yidadaa/ChatGPT-Next-Web/commits?per_page=1`;
