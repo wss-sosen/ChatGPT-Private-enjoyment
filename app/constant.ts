7c0a9cce339e454ceadc7ab2cd9da47a71c1f32e
sosen <127080498+wss-sosen@users.noreply.github.com>
2023-04-08 14:19:25 +0800

    增加版本通道

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
 export const OWNER = "Yidadaa";
 export const REPO = "ChatGPT-Next-Web";
 export const REPO_URL = `https://www.infoepoch.cn`;
-export const UPDATE_URL = `https://www.infoepoch.cn`;
+export const UPDATE_URL = `https://chat.infoepoch.cn`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/Yidadaa/ChatGPT-Next-Web/commits?per_page=1`;
